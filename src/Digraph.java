7c30adf2ea7a111c36a7ef5b752b34e3097cabfc
Griffin Ciluffo <griffin@ciluffo.com>
2015-11-20 09:19:26 -0700

    partially working BFS only visted nodes once

--- src/Digraph.java
+++ src/Digraph.java
@@ -1,17 +1,12 @@
-import java.awt.Point;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 /*Author: Griffin Ciluffo
   Date: November 18th 2015
@@ -21,8 +16,15 @@ public class Digraph {
 
 
 	private final static LinkedHashMap<Vertex, ArrayList<Vertex>> mGraph = new LinkedHashMap<Vertex, ArrayList<Vertex>>();
-	private ArrayList<Vertex> storage;
-	public final static boolean reverseMode = false;
+	private static ArrayList<Vertex> storage;
+	public static boolean reverseMode = false;
+	public static boolean reset = false;
+	private static Vertex temp;
+	private static Vertex nextCircledNode;
+	private static Queue<Vertex> myQ = new LinkedList<Vertex>();
+	private static boolean containsCircleNode = false;
+	
+
 
 	public Digraph(){
 
@@ -50,6 +52,8 @@ public class Digraph {
 				Vertex v1 = new Vertex(row, col, color, circle, direction, visited);
 				addNode(v1);
 				storage.add(v1);
+				//keep track of circled nodes
+
 
 				//add a flipped node to the graph
 				direction = flipDirection(direction);
@@ -69,7 +73,7 @@ public class Digraph {
 			Vertex key = entry.getKey();
 			//System.out.println(key);
 			ArrayList<Vertex> temp2  = createAdj(key);
-			
+
 			//System.out.println(temp2);
 			for(Vertex i: temp2){
 
@@ -77,12 +81,7 @@ public class Digraph {
 			}
 		}
 
-		
-		printMap();
-
-
 	}
-
 
 	public void addEdge(Vertex start, Vertex dest) {
 		/* Confirm both endpoints exist. */
@@ -322,7 +321,7 @@ public class Digraph {
 	}
 
 
-	public void printMap() {
+	public static void printMap() {
 		for(Map.Entry<Vertex,ArrayList<Vertex>> entry : mGraph.entrySet()) {
 			Vertex key = entry.getKey();
 			ArrayList<Vertex> value = entry.getValue();
@@ -331,33 +330,129 @@ public class Digraph {
 		}
 	}
 
+
+	public static Vertex getFlippedNode(Vertex v){
+
+		//record position of unflipped node
+		int pos = new ArrayList<Vertex>(mGraph.keySet()).indexOf(v);
+		//get list of all key nodes
+		ArrayList<Vertex> keys = new ArrayList<Vertex>(mGraph.keySet());
+		Vertex flipped = keys.get(pos + 1);
+
+
+
+		return flipped;
+	}
+
+	public static void engageReverseMode(){
+
+		if(reverseMode == false){
+			reverseMode = true;
+			reset = true;
+		}
+		else 
+			reverseMode = false;
+
+	}
+
 	
-	public static void BFS(Vertex root, LinkedHashMap<Vertex, ArrayList<Vertex>> map) {
-	    Deque<Vertex> myQ = new LinkedList<Vertex>();
-	    myQ.add(root);
-	    while(!myQ.isEmpty()) {
-	        Vertex current = myQ.getFirst();
-	        current.visited = true;
-	        //System.out.println("(" + current.row + ", " + current.col + ")");
-	        // Or you can store a set of visited vertices somewhere
-	        ArrayList<Vertex> neighbors = map.get(current);
-	        for (Vertex neighbor : neighbors) {
-	            if (!neighbor.visited) {
-	                myQ.addLast(neighbor);
-	            }
-	        }
-	    }
+	public static void BFS(Vertex root) {
+
+		myQ.add(root);
+		//traditional bfs goes until queue is empty, but search will end until the target it reached (7,7)
+		while(true) {
+			Vertex destination = storage.get(storage.size()-1);
+			Vertex current = myQ.remove();
+			current.visited = true;
+			//System.out.println("(" + current.row + ", " + current.col + ")" + " " + current.direction);
+			//System.out.println(myQ);
+			//if current node is a circle
+			if(current.isCircle != null && current.isCircle.equals("C"))
+				engageReverseMode();
+			if((current.row == 7 && current.col == 7) || myQ.contains(destination)) return;
+
+			//going in opposite direction 
+			if(reverseMode){
+
+				//get flipped vertex
+				current = getFlippedNode(current);
+				System.out.println("(" + current.row + ", " + current.col + ")" + " " + current.direction);
+				ArrayList<Vertex> neighbors = mGraph.get(current);
+				//reset the queue so that only flipped arrows are contained in it
+
+				//check if there is a circle arrow in the adj list, since we are clearing queues when is there a switch
+				for(Vertex i: neighbors) if(i.isCircle.equals("C")) containsCircleNode = true;
+
+				//if the current node is circle and there are no circles in the queue reset the queue
+				if(current.isCircle.equals("C") && !containsCircleNode){
+
+					//System.out.println(myQ);
+					temp = myQ.remove();
+					myQ.clear();
+
+				}
+
+				//else if the current node is a circle and the queue contains another circle, then wipe the queue and re add the circle
+				if(current.isCircle.equals("C") && containsCircleNode){
+					myQ.clear();
+					myQ.add(nextCircledNode);
+					containsCircleNode = false;
+				}
+
+				//in some cases the adj list will return an empty list, so revert back to the last point that was visited
+				if(neighbors.isEmpty())
+					myQ.add(temp);
+
+			
+
+				//add adj's to queue
+				for (Vertex neighbor : neighbors) {
+					if (!neighbor.visited) 
+						myQ.add(neighbor);	
+					if(neighbor.isCircle.equals("C"))
+						nextCircledNode = neighbor;
+				}
+			}
+			//going in regular direction
+			else{
+				//print the current node and its direction
+				System.out.println("(" + current.row + ", " + current.col + ")" + " " + current.direction);
+				ArrayList<Vertex> neighbors = mGraph.get(current);
+				if(current.isCircle.equals("C")){
+
+					//temp = myQ.remove();
+					myQ.clear();
+					reset = false;
+				}
+				//add the last visited point if there are no points contained in 
+				else if(neighbors.isEmpty()){
+
+					myQ.add(temp);
+
+				}
+
+				//add adj's tp queue
+				for (Vertex neighbor : neighbors) {
+					if (!neighbor.visited) 
+						myQ.add(neighbor);	 
+					if(neighbor.isCircle.equals("C"))
+						nextCircledNode = neighbor;
+				}
+
+				System.out.println(myQ);
+			}
+
+		}
 	}
 
 	public static void main(String[] args) {
 
 		Digraph theG = new Digraph();
-		Vertex root = new Vertex(1, 1, 'R', "N", "E", false);
-		theG.BFS(root, mGraph);
-		
+		Vertex root = theG.storage.get(0);
+		BFS(root);
 	}
-	
+
 }
-		
+
 
 
